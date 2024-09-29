a116b41407efbbad0baf952423e2026fea36923b
Mahmoudbaky <ma7mouudbaky@gmail.com>
2024-09-29 12:28:48 +0300

    fixing the routing issue with vercel

--- src/pages/OrderNow.jsx
+++ src/pages/OrderNow.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { ContactInfo, OurLocation, Footer } from "../sections";
 import NavBar from "../components/NavBar";
+import { useEffect } from "react";
 
 // import AnimatedPage from "../components/AnimatedPage";
 
