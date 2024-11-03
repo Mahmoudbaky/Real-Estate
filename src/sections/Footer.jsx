970cb5fbacf150c11727319aa8c205407af41f93
Mahmoudbaky <ma7mouudbaky@gmail.com>
2024-11-03 19:52:19 +0300

    fixing respnosive

--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Footer = () => {
   return (
     <section>
-      <footer className="bg-[#2cc3c3] text-white text-center p-4">
+      <footer className="bg-[#2cc3c3] text-white text-center p-4 w-full">
         <p>&copy; 2024 Madakheel. All rights reserved.</p>
       </footer>
     </section>
